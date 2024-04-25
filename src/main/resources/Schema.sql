75ecc7a13faf43b1b34a9f8ac3e451907e2afee5
Kjetil Hoem Borgan <kjbor1207@oslomet.no>
2024-04-25 18:09:54 +0200

    Adding Schema.sql

---
+++ src/main/resources/Schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Cinema (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    number INTEGER NOT NULL,
+    movie VARCHAR(255) not null,
+    amount INTEGER not null,
+    PRIMARY KEY (id)
+);
